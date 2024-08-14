48c7d603b952959844c092c9d72063befe4fa046
Tallyson Freitas <tallyson.eu@outlook.com>
2024-08-14 11:20:42 -0300

    style: color rematch

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,11 +18,11 @@ export default function RootLayout({
     return (
         <>
             <html lang="en">
-                <body
-                    className={`${inter.className} bg-gray-50 from-gray-50 text-black`}
-                >
-                    <Header />
-                    {children}
+                <body className={`${inter.className}`}>
+                    <div className="bg-gradient-to-b from-gray-50 to-gray-50 text-black">
+                        <Header />
+                        {children}
+                    </div>
                 </body>
             </html>
         </>
